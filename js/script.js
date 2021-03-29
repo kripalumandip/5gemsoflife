465844f0000f0268024c702cfd8956f1e082f0b8
Mandip Dhakal <75533098+kripalumandip@users.noreply.github.com>
2021-03-29 12:20:46 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,88 @@
+
+let para1 = document.getElementById('para1');
+para1.style.display = "none"
+let para2 = document.getElementById('para2');
+para2.style.display = "none"
+let para3 = document.getElementById('para3');
+para3.style.display = "none"
+let para4 = document.getElementById('para4');
+para4.style.display = "none"
+let para5 = document.getElementById('para5');
+para5.style.display = "none"
+
+
+
+function showhide1(){
+    let para1 = document.getElementById('para1');
+let btn1 = document.getElementById('btn1');
+
+
+   if(para1.style.display === "none"){
+    para1.style.display = "block";
+    btn1.innerHTML = "Show less"
+   }
+   else{
+    para1.style.display = "none"
+    btn1.innerHTML = "Show more"  
+   }
+   }
+function showhide2(){
+    let para2 = document.getElementById('para2');
+let btn2 = document.getElementById('btn2');
+
+
+   if(para2.style.display === "none"){
+    para2.style.display = "block";
+    btn2.innerHTML = "Show less"
+   }
+   else{
+    para2.style.display = "none"
+    btn2.innerHTML = "Show more"  
+   }
+   }
+function showhide3(){
+    let para3 = document.getElementById('para3');
+let btn3 = document.getElementById('btn3');
+
+
+   if(para3.style.display === "none"){
+    para3.style.display = "block";
+    btn1.innerHTML = "Show less"
+   }
+   else{
+    para3.style.display = "none"
+    btn3.innerHTML = "Show more"  
+   }
+   }
+function showhide4(){
+    let para4 = document.getElementById('para4');
+let btn4 = document.getElementById('btn4');
+
+
+   if(para4.style.display === "none"){
+    para4.style.display = "block";
+    btn4.innerHTML = "Show less"
+   }
+   else{
+    para4.style.display = "none"
+    btn4.innerHTML = "Show more"  
+   }
+   }
+function showhide5(){
+    let para5 = document.getElementById('para5');
+let btn5 = document.getElementById('btn5');
+
+
+   if(para5.style.display === "none"){
+    para5.style.display = "block";
+    btn5.innerHTML = "Show less"
+   }
+   else{
+    para5.style.display = "none"
+    btn5.innerHTML = "Show more"  
+   }
+   }
+
+   
+
+
